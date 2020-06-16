6e04612d082284fa7e8e3e1d91c6a07de535faba
Adriano Carnaroli <adriano.carnaroli@zarpsystem.com.br>
2020-06-16 13:00:39 -0300

    Implementando o fluxo de Coroutines.

--- app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/programacaoconcorrenteui/CoroutinesActivity.kt
@@ -2,11 +2,25 @@ package br.edu.ifsp.scl.programacaoconcorrenteui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_programacao_concorrente.*
+import kotlinx.coroutines.*
 
 class CoroutinesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutines)
+        supportActionBar?.title = "Coroutines"
+
+        val carregaImagensThread = Thread { run() }
+        carregaImagensThread.start()
+    }
+
+    private fun run() {
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(ProgramacaoConcorrenteActivity.Constantes.CARREGA_IMAGENS_DELAY)
+            altaIv.setImageResource(R.drawable.android_preto)
+            baixaIv.setImageResource(R.drawable.android_verde)
+        }
     }
 }
